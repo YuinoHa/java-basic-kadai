d21ad320178ccae5e95ca9dae667009148c412e5
YuinoHa <nohay0203@gmail.com>
2024-05-02 05:22:22 +0900

    課題004の作成

---
+++ src/kadai_004/Score_Chapter04.java
@@ -0,0 +1,35 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int score1 = 10;
+		int score2 = 20;
+		int score3 = 30;
+		int score4 = 40;
+		int score5 = 50;
+		int score6 = 60;
+		int score7 = 70;
+		int score8 = 80;
+		int score9 = 90;
+		int score10 = 100;
+		
+		int totalScore = score1 + score2 + score3 + score4 + score5 + score6 + score7 + score8 + score9 + score10;
+		int averageScore = totalScore / 10;
+		
+		System.out.println("Aさんの点数は" + score1 + "点");
+		System.out.println("Bさんの点数は" + score2 + "点");
+		System.out.println("Cさんの点数は" + score3 + "点");
+		System.out.println("Dさんの点数は" + score4 + "点");
+		System.out.println("Eさんの点数は" + score5 + "点");
+		System.out.println("Fさんの点数は" + score6 + "点");
+		System.out.println("Gさんの点数は" + score7 + "点");
+		System.out.println("Hさんの点数は" + score8 + "点");
+		System.out.println("Iさんの点数は" + score9 + "点");
+		System.out.println("Jさんの点数は" + score10 + "点");
+		
+		System.out.println("テストの平均点は" + averageScore + "点");
+	}
+
+}
