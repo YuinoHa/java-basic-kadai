3ac8df54e3550b3fbe9c7c7b450a24d9388cac1c
YuinoHa <nohay0203@gmail.com>
2024-05-12 16:35:27 +0900

    課題17の作成

---
+++ src/kadai_017/KatoExec_Chapter17.java
@@ -0,0 +1,23 @@
+package kadai_017;
+
+public class KatoExec_Chapter17 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		KatoTaro_Chapter17 KatoTaro = new KatoTaro_Chapter17();
+		KatoIchiro_Chapter17 KatoIchiro = new KatoIchiro_Chapter17();
+		KatoHanako_Chapter17 KatoHanako = new KatoHanako_Chapter17();
+		
+		KatoTaro.setGivenName();
+		KatoIchiro.setGivenName();
+		KatoHanako.setGivenName();
+		
+		KatoTaro.execIntroduce();
+		KatoIchiro.execIntroduce();
+		KatoHanako.execIntroduce();
+		
+		
+	}
+
+}
